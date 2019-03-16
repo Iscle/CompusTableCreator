f5bd01697d526d4b794761ed09569cebe0187dc9
Iscle <albertiscle9@gmail.com>
2019-03-16 19:09:18 +0100

    Updated so that it works correctly

--- src/CompusTableCreator.java
+++ src/CompusTableCreator.java
@@ -1,27 +1,10 @@
-import java.util.Scanner;
-
 public class CompusTableCreator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Insert the start value: ");
-        int startValue = sc.nextInt();
+        double step = (181.0 / 256.0);
 
-        System.out.print("Insert the end value: ");
-        int endValue = sc.nextInt();
-
-        for (int i = 0; i < startValue; i++) {
-            System.out.println("DB 0x00, 0x00");
-        }
-
-        int step = (endValue - startValue) / 180;
-
-        for (int i = startValue; i < endValue; i++) {
-            System.out.println("DB 0x" + Long.toHexString(step * (i - startValue)) + ", 0x" + Long.toHexString(step * ((i + 1) - startValue)));
-        }
-
-        for (int i = endValue; i < 256; i++) {
-            System.out.println("DB 0x00, 0x00");
+        for (int i = 0; i < 256; i += 2) {
+            System.out.println("DB 0x" + Long.toHexString(Math.round(step * i)) + " 0x" + Long.toHexString(Math.round(step * (i + 1))));
         }
 
     }
