4c5063b1edf21bf2134cc27445a709cf2db055a4
Iscle <albertiscle9@gmail.com>
2019-03-20 09:41:46 +0100

    Added code for the servo position

--- src/CompusTableCreator.java
+++ src/CompusTableCreator.java
@@ -1,10 +1,16 @@
 public class CompusTableCreator {
     public static void main(String[] args) {
 
-        double step = (181.0 / 256.0);
+        double step1 = (181.0 / 256.0);
 
         for (int i = 0; i < 256; i += 2) {
-            System.out.println("DB 0x" + Long.toHexString(Math.round(step * i)) + " 0x" + Long.toHexString(Math.round(step * (i + 1))));
+            //System.out.println("DB 0x" + Long.toHexString(Math.round(step1 * i)) + ", 0x" + Long.toHexString(Math.round(step1 * (i + 1))));
+        }
+
+        double step2 = (181.0/ 168.0);
+
+        for (int i = 0; i < 168; i += 2) {
+            System.out.println("DB 0x" + Long.toHexString(Math.round(step2 * i)) + ", 0x" + Long.toHexString(Math.round(step2 * (i + 1))));
         }
 
     }
